df01295ecf794c3867411b94c2df251c0e0361a4
Jan Ptáček <jan.ptacek@educhem.cz>
2022-10-14 08:47:28 +0200

    hovno druhy verze

--- kocka/Program.cs
+++ kocka/Program.cs
@@ -7,10 +7,21 @@ bod bodB = new bod();
 bodB.sourX = 4;
 bodB.sourY = -3;
 
-
+int Vzdalenost = bodA.Vzdalenost(bodB);
+Console.WriteLine("vzdalenost bodu je: " + Vzdalenost);
 
 class bod
 {
     public int sourX;
     public int sourY;
+
+    public int Vzdalenost(bod druhyBod)
+    {
+        double vzdX = Math.Pow(druhyBod.sourX - this.sourX, 2);
+        double vzdY = Math.Pow(druhyBod.sourY - this.sourY, 2);
+        double odm = Math.Sqrt(vzdX + vzdY);
+
+        double vysledek = Math.Abs(odm);
+        return (int)vysledek;
+    }
 }
