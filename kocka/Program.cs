afb961b33fbf9f6d186bef9facc3bfa21dd1baf3
jan.ptacek <jan.ptacek@VT1-15>
2022-10-14 08:24:56 +0200

    Přidejte soubory projektu.

---
+++ kocka/Program.cs
@@ -0,0 +1,16 @@
+﻿
+bod bodA = new bod();
+bodA.sourX = 3;
+bodA.sourY = -4;
+
+bod bodB = new bod();
+bodB.sourX = 4;
+bodB.sourY = -3;
+
+
+
+class bod
+{
+    public int sourX;
+    public int sourY;
+}
